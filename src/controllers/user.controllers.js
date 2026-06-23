7081c24c7b3e1a4d1088012f82a462dc95d80522
Ritesh Tiwari <riteshtiwari9651270995@gmail.com>
2026-06-23 13:42:13 +0530

    just postman testing

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,10 @@
+import {asyncHandler} from  "./utils/asyncHandler.js"
+
+const registerUser = asyncHandler(async (req, res) =>{
+      
+      const {fullName, email, username, password } = req.body
+      console.log("email: ", email);
+
+})
+
+export {registerUser}
